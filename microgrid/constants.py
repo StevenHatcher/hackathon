1f88cd28505c8479d36b176785d6f07835d9a649
Janice Cotcher <42756980+janice-cotcher@users.noreply.github.com>
2025-03-15 19:14:18 +0000

    update constants.py

--- microgrid/constants.py
+++ microgrid/constants.py
@@ -11,4 +11,13 @@ WATT = 1
 GIGAJOULE = 1e9
 KWH = WATT * SECONDS * MINUTES * 1000
 GIGAJOULE_KWH = GIGAJOULE / KWH
-print(GIGAJOULE_KWH)
+# print(GIGAJOULE_KWH)
+
+# Weight in KG
+lb_to_kg = 0.453592
+
+# solar panels
+SOLAR_SPECS = {
+    {"60 Cell": {"size": [3, 5.5], "energy KWH": 5.775, "weight": 40}},
+    {"other": {"size": [1, 1], "energy KWH": 6, "weight": 30}},
+}
